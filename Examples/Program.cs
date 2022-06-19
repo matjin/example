afec801af998703f9674dd27172889f0006e15e5
Matthew Jin <matjin@microsoft.com>
2022-06-19 10:09:43 -0700

    Revert "good"
    This reverts commit 265be1beb3ef369d489d12c7564b20c1319e4030.

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -12,38 +12,6 @@ namespace Examples
     
     public class Program
     {
-        // Instantiate a StreamWriter instance.
-        StreamWriter SWGlobal = new StreamWriter("everwhat.txt");
-        
-
-        public void NullDerefOK1()
-        {
-            int x = 4;
-            while(x == 4)
-            {
-                Console.WriteLine("HELLO WORLD!!");
-            }
-            object c = null;
-            c.GetHashCode();
-
-        }
-
-        public void NullDerefOK2()
-        {
-            var x = new object();
-            if (x == null)
-            {
-                object c = null;
-                c.GetHashCode();
-            }
-        }
-
-        public void NullDerefBad1()
-        {
-            Program x = null;
-            var y = x?.SWGlobal;
-            y.GetHashCode();
-        }
     }
 
     public class MainClass {
