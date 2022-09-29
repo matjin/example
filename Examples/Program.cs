2359be94a660b18e796fef2d88659b8ebc2e79db
Matthew Jin <matjin@microsoft.com>
2022-09-29 01:22:22 -0700

    Revert "cleanup"
    This reverts commit 8334049f88573a2b9497f26b1a33172dff6567d9.

---
+++ Examples/Program.cs
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace Examples
+{
+    
+    public class Program
+    {
+    }
+
+    public class MainClass {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
